ed63d3048fd8b2d6a7d50173e9231933736f72c7
John Dungao <johnmiguel.dungao@gmail.com>
2024-03-22 06:27:20 -0400

    Update .gitignore and add updateProduct route

--- server/routes/product.js
+++ server/routes/product.js
@@ -35,6 +35,22 @@ const deleteProduct = async (id) => {
   await client.query(SQL, [id]);
 };
 
+const updateProduct = async (
+  id,
+  { name, descriptions, price, stock_quantity, image_url }
+) => {
+  const SQL = `UPDATE products SET name=$1, descriptions=$2, price=$3, stock_quantity=$4, image_url=$5 WHERE id=$6 RETURNING *`;
+  const response = await client.query(SQL, [
+    name,
+    descriptions,
+    price,
+    stock_quantity,
+    image_url,
+    id,
+  ]);
+  return response.rows[0];
+};
+
 // <-------- Routes -------->
 
 router.get("/", async (req, res) => {
@@ -57,6 +73,15 @@ router.delete("/:id", isAdmin, async (req, res) => {
   try {
     await deleteProduct(req.params.id);
     res.status(204).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.put("/:id", isAdmin, async (req, res) => {
+  try {
+    const product = await updateProduct(req.params.id, req.body);
+    res.status(200).json(product);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
