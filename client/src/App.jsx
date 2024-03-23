86c0172023c0dea306e14566cbfc3e43700e6d9d
stevecoding100 <98184564+stevecoding100@users.noreply.github.com>
2024-03-23 19:09:06 -0400

    Revert "Streaming feature"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import StreamingHomePage from "./pages/StreamingHomePage";
 import MerchandiseHomePage from "./pages/MerchandiseHomePage";
-import SignUpPage from "./pages/SignUpPage";
-import AnimeDetailsPage from "./pages/AnimeDetailsPage";
+
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<StreamingHomePage />} />
-      <Route path="/shop" element={<MerchandiseHomePage />} />
-      <Route path="/register" element={<SignUpPage />} />
-      <Route path="/series/:id" element={<AnimeDetailsPage />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<StreamingHomePage />} />
+            <Route path="/shop" element={<MerchandiseHomePage />} />
+        </Routes>
+    );
 };
 
 export default App;
--- client/src/pages/StreamingHomePage.jsx
+++ client/src/pages/StreamingHomePage.jsx
@@ -1,40 +1,13 @@
 import CarouselSlider from "../components/CarouselSlider";
 import Navbar from "../components/Navbar";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import AnimeCard from "../components/AnimeCard";
-AnimeCard;
+
 const StreamingHomePage = () => {
-  const url = "https://zenkai-api.vercel.app/anime/gogoanime/top-airing";
-  const [animeList, setAnimeList] = useState([]);
-
-  const getAnimeList = async () => {
-    try {
-      const { data } = await axios.get(url, { params: { page: 1 } });
-      setAnimeList(data.results);
-    } catch (err) {
-      throw new Error(err.message);
-    }
-  };
-
-  useEffect(() => {
-    getAnimeList();
-  }, []);
-
-  return (
-    <div className="bg-[#000000] min-h-screen w-full">
-      <Navbar pageType="streaming" />
-      <div className="container mx-auto py-8">
-        <h1 className="text-4xl font-bold text-white mb-8">Anime List</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {console.log(animeList)}
-          {animeList.map((anime) => (
-            <AnimeCard key={anime.id} anime={anime} />
-          ))}
+    return (
+        <div className="bg-[#000000] h-[100vh] w-full">
+            <Navbar pageType="streaming" />
+            <CarouselSlider />
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default StreamingHomePage;
