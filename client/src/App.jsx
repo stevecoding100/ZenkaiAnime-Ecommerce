9e8ab716c8f8cfd26ede3888bdfd6175b634c78d
John Dungao <johnmiguel.dungao@gmail.com>
2024-03-23 01:50:32 -0400

    add signup page, and add route to /register in signup button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import StreamingHomePage from "./pages/StreamingHomePage";
 import MerchandiseHomePage from "./pages/MerchandiseHomePage";
+import SignUpPage from "./pages/SignUpPage";
 
 const App = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<StreamingHomePage />} />
-            <Route path="/shop" element={<MerchandiseHomePage />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<StreamingHomePage />} />
+      <Route path="/shop" element={<MerchandiseHomePage />} />
+      <Route path="/register" element={<SignUpPage />} />
+    </Routes>
+  );
 };
 
 export default App;
