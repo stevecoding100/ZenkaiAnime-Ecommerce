d8a1e1f1c70b93c6c5a8cb1110dff1e0c08dc8ca
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-03-28 01:53:05 -0400

    Added the AnimeDetailpage, EpisodeCard. It is Style and responsive

--- client/src/components/EpisodeCard.jsx
+++ client/src/components/EpisodeCard.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
-const EpisodeCard = () => {
-    return <div>EpisodeCard</div>;
+const EpisodeCard = ({ episode, animeId }) => {
+    const { id, number, url } = episode;
+    return (
+        <Link
+            to={`/${animeId}/watch/${episode.id}`}
+            className="block bg-black rounded-md p-4 hover:bg-blue-500 h-[8vh] md:h-[10vh]"
+        >
+            <div className="flex flex-row justify-between border-b-2">
+                <h2 className="text-xl  text-white font-bold">
+                    Episode {number}
+                </h2>
+            </div>
+            {/* Add more episode details */}
+        </Link>
+    );
 };
 
 export default EpisodeCard;
