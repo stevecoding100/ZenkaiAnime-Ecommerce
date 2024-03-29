675ecc3d5f01940a03d52a6fbab3128e24e8a5de
John Dungao <johnmiguel.dungao@gmail.com>
2024-03-29 11:24:58 -0400

    fix folder structure to make it more cleaner, communicate client signup/loginpage to backend login/register endpoint

---
+++ client/api/ecomAPI.js
@@ -0,0 +1,37 @@
+// const baseURL = import.meta.env.VITE_ECOMMERCE_API_URL;
+const baseURL = "http://localhost:3000/api";
+import axios from "axios";
+const ecomAPI = {
+  //AUTH
+  auth: {
+    register: async (formData) => {
+      try {
+        const user = await axios.post(`${baseURL}/auth/register`, formData);
+        return user;
+      } catch (error) {
+        throw new Error("Error registering user", error);
+      }
+    },
+    login: async (formData) => {
+      try {
+        const user = await axios.post(`${baseURL}/auth/login`, formData);
+        return user;
+      } catch (error) {
+        throw new Error("Error logging in user", error);
+      }
+    },
+  },
+  //PRODUCTS
+  products: {
+    getProducts: async () => {
+      try {
+        const products = await axios.get(`${baseURL}/products`);
+        return products;
+      } catch (error) {
+        throw new Error("Error getting products", error);
+      }
+    },
+  },
+};
+
+export default ecomAPI;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -61,6 +61,7 @@ const updateUser = async (
 router.post("/register", async (req, res) => {
   try {
     const user = await createUser(req.body);
+
     res.status(201).json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
