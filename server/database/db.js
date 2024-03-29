fe41b7e23a66ee41136a5293cabab0b398fc0e1e
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-03-29 15:33:05 -0400

    saved

---
+++ server/database/db.js
@@ -0,0 +1,96 @@
+const pg = require("pg");
+require("dotenv").config();
+const client = new pg.Client(process.env.DATABASE_URL);
+
+const createTable = async () => {
+  const SQL = `
+            -- Zenkai DB
+
+            DROP TABLE IF EXISTS watchlists;
+            DROP TABLE IF EXISTS favorites;
+            DROP TABLE IF EXISTS cart_items;
+            DROP TABLE IF EXISTS order_items;
+            DROP TABLE IF EXISTS carts;
+            DROP TABLE IF EXISTS products;
+            DROP TABLE IF EXISTS orders;
+            DROP TABLE IF EXISTS users;
+            DROP TYPE IF EXISTS watch_status;
+            DROP TYPE IF EXISTS order_status;
+
+            CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+            CREATE TYPE watch_status AS ENUM ('watching', 'completed', 'on-hold', 'dropped', 'plan-to-watch');
+            CREATE TYPE order_status AS ENUM ('pending', 'shipped', 'delivered', 'cancelled');
+
+            -- Table users
+            CREATE TABLE users (
+                id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                first_name VARCHAR(50) NOT NULL,
+                last_name VARCHAR(50) NOT NULL,
+                username VARCHAR(50) UNIQUE,
+                email VARCHAR(255) UNIQUE,
+                password VARCHAR(255),
+                billing_info JSON,
+                is_admin BOOLEAN DEFAULT FALSE
+            );
+
+            -- Anime Service
+            CREATE TABLE watchlists (
+                id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+                anime_name VARCHAR(255),
+                status watch_status DEFAULT 'plan-to-watch',
+                progress INT,
+                rating INT
+            );
+
+            CREATE TABLE favorites (
+                id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+                anime_name VARCHAR(255)
+            );
+
+            -- Ecommerce
+            CREATE TABLE products (
+                id SERIAL PRIMARY KEY,
+                name TEXT,
+                descriptions TEXT,
+                price DECIMAL(10,2),
+                stock_quantity INT,
+                image_url VARCHAR(255)
+            );
+
+            CREATE TABLE carts (
+                id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                user_id UUID REFERENCES users(id) ON DELETE CASCADE
+            );
+
+            CREATE TABLE cart_items (
+                id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                cart_id UUID REFERENCES carts(id) ON DELETE CASCADE,
+                product_id INT REFERENCES products(id) ON DELETE CASCADE,
+                quantity INT
+            );
+
+            CREATE TABLE orders (
+                id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+                total_price DECIMAL,
+                order_date DATE DEFAULT CURRENT_DATE,
+                status order_status DEFAULT 'pending'
+            );
+
+            CREATE TABLE order_items (
+                id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                order_id UUID REFERENCES orders(id) ON DELETE CASCADE,
+                product_id INT REFERENCES products(id) ON DELETE CASCADE,
+                quantity INT,
+                price DECIMAL
+            );
+            `;
+  await client.query(SQL);
+};
+
+module.exports = {
+  client,
+  createTable,
+};
---
+++ server/routes/product.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const router = express.Router();
+const { client } = require("../database/db");
+const { isAdmin } = require("../middlewares/authMiddleware");
+
+// <-------- Database Queries -------->
+
+// Create a new product
+const createProduct = async ({
+  name,
+  descriptions,
+  price,
+  stock_quantity,
+  image_url,
+}) => {
+  const SQL = `INSERT INTO products (name, descriptions, price, stock_quantity, image_url) VALUES ($1, $2, $3, $4, $5) RETURNING *`;
+  const response = await client.query(SQL, [
+    name,
+    descriptions,
+    price,
+    stock_quantity,
+    image_url,
+  ]);
+  return response.rows[0];
+};
+
+// Get all products
+const getAllProducts = async () => {
+  const SQL = `SELECT * FROM products`;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+// Delete a product
+const deleteProduct = async (id) => {
+  const SQL = `DELETE FROM products WHERE id = $1`;
+  await client.query(SQL, [id]);
+};
+// Update a product
+const updateProduct = async (
+  id,
+  { name, descriptions, price, stock_quantity, image_url }
+) => {
+  const SQL = `UPDATE products SET name=$1, descriptions=$2, price=$3, stock_quantity=$4, image_url=$5 WHERE id=$6 RETURNING *`;
+  const response = await client.query(SQL, [
+    name,
+    descriptions,
+    price,
+    stock_quantity,
+    image_url,
+    id,
+  ]);
+  return response.rows[0];
+};
+
+// <-------- Routes -------->
+
+//Base route /api/products
+router.get("/", async (req, res) => {
+  try {
+    const products = await getAllProducts();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// <-- Admin Routes  -->
+
+// <!-- Create a new product
+router.post("/", isAdmin, async (req, res) => {
+  try {
+    const product = await createProduct(req.body);
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// <!-- Delete a product
+
+router.delete("/:id", isAdmin, async (req, res) => {
+  try {
+    await deleteProduct(req.params.id);
+    res.status(204).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// <!-- Update a product
+router.put("/:id", isAdmin, async (req, res) => {
+  try {
+    const product = await updateProduct(req.params.id, req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
