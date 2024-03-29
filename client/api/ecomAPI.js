37b8116f9c553866fa56afc7fe146647e2d88441
John Dungao <johnmiguel.dungao@gmail.com>
2024-03-29 11:24:58 -0400

    fix folder structure to make it more cleaner, communicate client signup/loginpage to backend login/register endpoint

--- client/api/ecomAPI.js
+++ client/api/ecomAPI.js
@@ -1,15 +1,15 @@
-const baseURL = import.meta.env.VITE_ECOMMERCE_API_URL;
+// const baseURL = import.meta.env.VITE_ECOMMERCE_API_URL;
+const baseURL = "http://localhost:3000/api";
 import axios from "axios";
 const ecomAPI = {
     //AUTH
     auth: {
         register: async (formData) => {
             try {
-                const { user } = await axios.post(
+                const user = await axios.post(
                     `${baseURL}/auth/register`,
                     formData
                 );
-                localStorage.setItem("token", user.token);
                 return user;
             } catch (error) {
                 throw new Error("Error registering user", error);
@@ -21,7 +21,6 @@ const ecomAPI = {
                     `${baseURL}/auth/login`,
                     formData
                 );
-                localStorage.setItem("token", user.data.token);
                 return user;
             } catch (error) {
                 throw new Error("Error logging in user", error);
@@ -36,84 +35,6 @@ const ecomAPI = {
                 return products;
             } catch (error) {
                 throw new Error("Error getting products", error);
-            }
-        },
-        createProduct: async (formData) => {
-            try {
-                const product = await axios.post(
-                    `${baseURL}/products`,
-                    formData,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem(
-                                "token"
-                            )}`,
-                        },
-                    }
-                );
-                return product;
-            } catch (error) {
-                throw new Error("Error creating product", error);
-            }
-        },
-        deleteProduct: async (id) => {
-            try {
-                const product = await axios.delete(
-                    `${baseURL}/products/${id}`,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem(
-                                "token"
-                            )}`,
-                        },
-                    }
-                );
-                return product;
-            } catch (error) {
-                throw new Error("Error deleting product", error);
-            }
-        },
-        updateProduct: async (id, formData) => {
-            try {
-                const product = await axios.put(
-                    `${baseURL}/products/${id}`,
-                    formData,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem(
-                                "token"
-                            )}`,
-                        },
-                    }
-                );
-                return product;
-            } catch (error) {
-                throw new Error("Error updating product", error);
-            }
-        },
-    },
-    //CART
-    cart: {
-        getCart: async (user_id) => {
-            try {
-                const cart = await axios.get(`${baseURL}/cart/add${user_id}`, {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem(
-                            "token"
-                        )}`,
-                    },
-                });
-                return cart;
-            } catch (error) {
-                throw new Error("Error getting cart", error);
-            }
-        },
-        addToCart: async (formData) => {
-            try {
-                const cart = await axios.post(`${baseURL}/cart`, formData);
-                return cart;
-            } catch (error) {
-                throw new Error("Error adding to cart", error);
             }
         },
     },
