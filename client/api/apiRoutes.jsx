9ebcbfe2300b716fb691b791ffadb627253bad48
John Dungao <johnmiguel.dungao@gmail.com>
2024-03-29 11:24:58 -0400

    fix folder structure to make it more cleaner, communicate client signup/loginpage to backend login/register endpoint

--- client/api/apiRoutes.jsx
+++ client/api/apiRoutes.jsx
@@ -1,9 +1,10 @@
 const baseURL = import.meta.env.VITE_API_URL;
 const apiRoutes = {
   searchAnime: (q) => `${baseURL}/${q}`,
-  getAnimeInfo: (id) => `${baseURL}/info/${id}?provider=zoro`, // must provide gogoanime as provider [params]
+  getAnimeInfo: (id) => `${baseURL}/info/${id}?provider=gogoanime`, // must provide gogoanime as provider [params]
   getTrendingAnime: (page = 1) => `${baseURL}/trending?page=${page}&perPage=30`,
   getPopularAnime: () => `${baseURL}/popular`,
+  getStreamingLink: (id) => `${baseURL}/watch/${id}`,
   proxyConfig: {
     protocol: "https",
     host: import.meta.env.VITE_PROXY_HOST,
