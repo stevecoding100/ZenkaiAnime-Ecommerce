606504b938b558ec47ca040b73dbf78eb9a5b301
Steve De La Rosa <stevedelarosa1036@gmail.com>
2024-03-30 14:23:24 -0400

    saved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,17 @@ import SignUpPage from "./pages/auth/SignUpPage";
 import AnimeDetailsPage from "./pages/streaming/AnimeDetailsPage";
 import LoginPage from "./pages/auth/LoginPage";
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<StreamingHomePage />} />
-        <Route path="/shop" element={<MerchandiseHomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/series/:animeId" element={<AnimeDetailsPage />} />
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<StreamingHomePage />} />
+                <Route path="/shop" element={<MerchandiseHomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/series/:animeId" element={<AnimeDetailsPage />} />
+            </Routes>
+        </>
+    );
 };
 
 export default App;
