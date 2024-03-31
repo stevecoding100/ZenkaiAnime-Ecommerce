2b3cbe040423bc7e338dd7dc85ac2ada5d40846a
John Dungao <johnmiguel.dungao@gmail.com>
2024-03-31 12:58:19 -0400

    Refactor API routes to remove CORS proxy

--- client/api/apiRoutes.jsx
+++ client/api/apiRoutes.jsx
@@ -1,13 +1,10 @@
 const baseURL = import.meta.env.VITE_API_URL;
 const apiRoutes = {
-  searchAnime: (q) => `https://cors-anywhere.herokuapp.com/${baseURL}/${q}`,
-  getAnimeInfo: (id) =>
-    `https://cors-anywhere.herokuapp.com/${baseURL}/info/${id}?provider=gogoanime`, // must provide gogoanime as provider [params]
+  searchAnime: (q) => `${baseURL}/${q}`,
+  getAnimeInfo: (id) => `${baseURL}/info/${id}?provider=gogoanime`, // must provide gogoanime as provider [params]
   getTrendingAnime: (page = 1) => `${baseURL}/trending?page=${page}&perPage=30`,
-  getPopularAnime: () =>
-    `$https://cors-anywhere.herokuapp.com/{baseURL}/popular`,
-  getStreamingLink: (id) =>
-    `https://cors-anywhere.herokuapp.com/${baseURL}/watch/${id}`,
+  getPopularAnime: () => `${baseURL}/popular`,
+  getStreamingLink: (id) => `${baseURL}/watch/${id}`,
   proxyConfig: {
     protocol: "https",
     host: import.meta.env.VITE_PROXY_HOST,
