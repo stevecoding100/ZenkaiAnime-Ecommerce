a809d4a24978ecdab793239762e0c2253294901d
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2024-04-03 12:20:54 -0400

    Add react-icons package and update AdminPage component

---
+++ client/src/components/admin/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+const Layout = () => {
+  return (
+    <div>
+      <main className="flex flex-row">
+        <Navbar />
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
