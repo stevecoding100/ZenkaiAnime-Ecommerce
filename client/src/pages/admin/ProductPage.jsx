8969c28c3fb69d723c5eac771218df86f6de008e
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2024-04-04 09:23:00 -0400

    Add product fetching functionality to ProductPage component and create getProductById route

--- client/src/pages/admin/ProductPage.jsx
+++ client/src/pages/admin/ProductPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 const ProductPage = () => {
+  const [product, setProduct] = useState({});// [1
   const { id } = useParams();
+
+    useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const product = await ecomAPI.products.getProduct(id);
+        setProduct(product.data);
+      } catch (error) {
+        console.log("Error fetching product", error);
+      }
+    };
+
   return <div>ProductPage{id}</div>;
 };
 
