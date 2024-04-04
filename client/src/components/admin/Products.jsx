a3edb33c6fba7619d3b22a885e04fa72f8cb801b
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2024-04-04 08:27:47 -0400

    style table

--- client/src/components/admin/Products.jsx
+++ client/src/components/admin/Products.jsx
@@ -32,40 +32,56 @@ const Products = () => {
   };
 
   return (
-    <div>
-      <div className="max-h-[500px]">
-        <table className="w-full border border-slate-400 table-auto">
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Price</th>
-              <th>Stock</th>
-              <th>Actions</th>
+    <div className="shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Product
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Price
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Stock
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentProducts.map((product) => (
+            <tr
+              key={product.id}
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+            >
+              <td className="px-6 py-4">
+                <Image
+                  src={product.image_url}
+                  alt={product.name}
+                  width={75}
+                  height={75}
+                />
+              </td>
+              <td className="px-6 py-4">{product.name}</td>
+              <td className="px-6 py-4">{product.price}</td>
+              <td className="px-6 py-4">{product.stock_quantity}</td>
+              <td className="px-6 py-4 text-right">
+                <button className="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-2">
+                  Edit
+                </button>
+                <button className="font-medium text-red-600 dark:text-red-500 hover:underline">
+                  Delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {currentProducts.map((product) => (
-              <tr key={product.id}>
-                <td>
-                  <Image
-                    src={product.image_url}
-                    alt={product.name}
-                    width={75}
-                    height={75}
-                  />
-                </td>
-                <td>{product.name}</td>
-                <td>{product.price}</td>
-                <td>{product.stock_quantity}</td>
-                <td>
-                  <button>Edit</button>
-                  <button>Delete</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
       <div className="flex justify-center mt-4">
         <Pagination
           total={Math.ceil(products.length / productsPerPage)}
