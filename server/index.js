646c2d02153a1d7750e9290d1fba635199051ea9
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2024-04-06 17:49:23 -0400

    Add CORS configuration for specific origin

--- server/index.js
+++ server/index.js
@@ -18,9 +18,11 @@ const animeRoutes = require("./routes/anime");
 
     const cors = require("cors");
     // Allow specific origin(s)
+
     app.use(
       cors({
         origin: "https://zenkai-anime.vercel.app",
+        optionsSuccessStatus: 200,
       })
     );
 
