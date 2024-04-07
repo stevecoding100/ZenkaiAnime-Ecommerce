8a3fc688ad1660965b50c46b6105aad56115fd4c
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2024-04-07 17:12:42 -0400

    Add CORS headers to allow requests from specific origin

--- server/index.js
+++ server/index.js
@@ -23,6 +23,16 @@ const animeRoutes = require("./routes/anime");
       })
     );
 
+    app.use((req, res, next) => {
+      res.header(
+        "Access-Control-Allow-Origin",
+        "https://zenkai-anime.vercel.app"
+      );
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      next();
+    });
+
     app.use("/", animeRoutes);
     app.use("/", authRoutes);
     app.use("/", productRoutes);
