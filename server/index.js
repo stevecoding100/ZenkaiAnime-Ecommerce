667a0c0611b47acdd3c3e8885ff5dc0b8a64a883
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2024-04-07 18:50:33 -0400

    Refactor server/index.js to use CORS and apply caching to animeRoutes

--- server/index.js
+++ server/index.js
@@ -1,45 +1,40 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const { client, seedData, createTable } = require("./database/db");
+const cors = require("cors");
 const apicache = require("apicache");
 let cache = apicache.middleware;
-const cors = require("cors");
+
 app.use(express.json());
 
-// Routes
+app.use(
+  cors({
+    origin: "https://zenkai-anime.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+
+// Import routes
 const authRoutes = require("./routes/auth");
 const productRoutes = require("./routes/product");
 const cartRoutes = require("./routes/cart");
 const orderRoutes = require("./routes/order");
 const animeRoutes = require("./routes/anime");
 
-(async () => {
-  try {
-    await client.connect();
+// Apply caching only to animeRoutes
+animeRoutes.use(cache("10 minutes"));
 
-    const corsOptions = {
-      origin: "https://zenkai-anime.vercel.app",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: "Content-Type,Authorization",
-    };
+// Use routes
+app.use("/", animeRoutes);
+app.use("/", authRoutes);
+app.use("/", productRoutes);
+app.use("/", cartRoutes);
+app.use("/", orderRoutes);
+app.use("/", (req, res) => {
+  res.json({ message: "Server is running!" });
+});
 
-    app.use(cors(corsOptions));
-    app.use(cache("10 minutes"));
-    app.use("/", animeRoutes);
-    app.use("/", authRoutes);
-    app.use("/", productRoutes);
-    app.use("/", cartRoutes);
-    app.use("/", orderRoutes);
-    app.use("/", (req, res) => {
-      res.json({ message: "Server is running!" });
-    });
-    app.listen(PORT, () => {
-      console.log(`Server is listening on port ${PORT}!`);
-    });
-  } catch (error) {
-    console.error("Error starting server!", error);
-  }
-})();
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}!`);
+});
