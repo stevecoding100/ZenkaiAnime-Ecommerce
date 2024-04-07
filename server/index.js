919a9bbea75619cd5d1e4b2b8201bd55d4eb655a
John Miguel Dungao <johnmiguel.dungao@gmail.com>
2024-04-07 19:18:11 -0400

    Update CORS configuration and apply caching to all routes in server/index.js

--- server/index.js
+++ server/index.js
@@ -19,11 +19,11 @@ animeRoutes.use(cache("5 minutes"));
 // productRoutes.use(cache("2 minutes"));
 
 // Use routes
-app.use("/", animeRoutes);
-app.use("/", authRoutes);
-app.use("/", productRoutes);
-app.use("/", cartRoutes);
-app.use("/", orderRoutes);
+app.use("/", cors(), animeRoutes);
+app.use("/", cors(), authRoutes);
+app.use("/", cors(), productRoutes);
+app.use("/", cors(), cartRoutes);
+app.use("/", cors(), orderRoutes);
 app.use("/", (req, res) => {
   res.json({ message: "Server is running!" });
 });
